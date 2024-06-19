e72508c669998cda2f9b9c770c2b02af70e3aed5
J-Jaeh <1wo2gur@naver.com>
2024-06-19 15:23:28 +0900

    #3 feat : socket dto

---
+++ src/status/DTO/status.DTO.ts
@@ -0,0 +1,24 @@
+export class playerJoinRoomDTO {
+
+  room: string;
+
+  x: string;
+
+  y: string;
+}
+
+
+export class playerMovementDTO {
+
+  x: string;
+
+  y: string;
+}
+
+export class playerAttackPositionDTO{
+
+  x:string;
+  
+  y:string;
+}
+
