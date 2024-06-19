44dfb0b4e67027bfb7711d69915d24175663435c
J-Jaeh <1wo2gur@naver.com>
2024-06-19 19:51:56 +0900

    #15 feat : event-emitter module  추가

--- src/status/status.module.ts
+++ src/status/status.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { statusGateway } from './status.gateway'
 import { StatusBombGameService } from './status.service';
+import { EventEmitter2 } from "@nestjs/event-emitter";
 
 @Module({
-    providers: [statusGateway,StatusBombGameService]
+    providers: [statusGateway,StatusBombGameService,EventEmitter2]
 })
 export class StatusModule {}
