49baa7b4a6c7e891ff5c7b914b46a8f3fd4d6f47
J-Jaeh <1wo2gur@naver.com>
2024-06-23 14:27:22 +0900

    #31 feat :공용  response DTO 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreatCommonUserDTO } from "./DTO/users.creatDTO";
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async registerUser(createUserDto: CreatCommonUserDTO) {
+    return this.usersRepository.createCommonUser(createUserDto);
+  }
+
 }
+
+
+
