5682fc17d9064ffc96ea9726880e38d0ac04c3cc
J-Jaeh <1wo2gur@naver.com>
2024-06-23 17:50:37 +0900

    #31 feat : jwt 토큰 관련 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,26 @@
-import { Module } from '@nestjs/common';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-import { StatusModule } from './status/status.module';
-import { statusGateway } from './status/status.gateway'
-import { StatusBombGameService } from './status/status.service';
-import { UsersModule } from './users/users.module';
-import { GuestModule } from './guest/guest.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import * as dotenv from 'dotenv';
+import { Module } from "@nestjs/common";
+import { EventEmitterModule } from "@nestjs/event-emitter";
+import { StatusModule } from "./status/status.module";
+import { statusGateway } from "./status/status.gateway";
+import { StatusBombGameService } from "./status/status.service";
+import { UsersModule } from "./users/users.module";
+import { GuestModule } from "./guest/guest.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from "dotenv";
 
 dotenv.config();
+
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI),
+    JwtModule.register({secret:process.env.JWT_SECRET, signOptions:{expiresIn:process.env.JWT_EXPIRE_TIME}}),
     StatusModule,
     EventEmitterModule.forRoot(),
     UsersModule,
-    GuestModule,
+    GuestModule
   ],
-  providers: [statusGateway,StatusBombGameService]
+  providers: [statusGateway, StatusBombGameService]
 })
-export class AppModule {}
+export class AppModule {
+}
