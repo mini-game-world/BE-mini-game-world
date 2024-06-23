d3757fdf91f06f646d415a97bd5652712ca270a8
NOAH <flhbigfamily7@gmail.com>
2024-06-23 19:11:14 +0900

    #34 feat: eventemitter를 app.module에서  status.module로 옮김

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { EventEmitterModule } from '@nestjs/event-emitter';
 import { StatusModule } from './status/status.module';
-import { statusGateway } from './status/status.gateway'
-import { StatusBombGameService } from './status/status.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { GuestModule } from './guest/guest.module';
@@ -14,11 +11,9 @@ dotenv.config();
   imports: [
     MongooseModule.forRoot(process.env.MONGO_URI),
     StatusModule,
-    EventEmitterModule.forRoot(),
     AuthModule,
     UsersModule,
-    GuestModule,
-  ],
-  providers: [statusGateway,StatusBombGameService]
+    GuestModule
+  ]
 })
 export class AppModule {}
