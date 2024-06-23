905be8dc6f0290c42aa88542c5c8bc024deb6f98
J-Jaeh <1wo2gur@naver.com>
2024-06-23 15:23:21 +0900

    #31 feat : 빌더패턴 적용 유저등록

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,27 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { UsersService } from './users.service'
-import { CreatCommonUserDTO } from './DTO/users.creatDTO'
+import { Body, Controller, HttpCode, Logger, Post } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { CreatCommonUserDTO } from "./DTO/users.creatDTO";
+import { ResponseDTO } from "../common/response.DTO";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
-  constructor(private readonly userService:UsersService) {
+  constructor(private readonly userService: UsersService) {
   }
+  private logger: Logger = new Logger("Users - Controller");
 
-  @Post('register')
+  @Post("register")
   @HttpCode(201)
-  registerUser(@Body() creatUserDTO:CreatCommonUserDTO){
-     const userName =  this.userService.registerUser(creatUserDTO)
-    return { "success":true , "nickname":userName}
+  async registerUser(@Body() creatUserDTO: CreatCommonUserDTO):Promise<ResponseDTO<{ nickname: string }>> {
+    const userName:string = await this.userService.registerUser(creatUserDTO);
+
+    const response = ResponseDTO.builder<{ nickname: string }>()
+      .setSuccess(true)
+      .setData({ nickname: userName })
+      .build();
+
+    this.logger.log( `response==> ${response}`)
+
+    return response;
   }
 
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersRepository } from './users.repository';
+import { User, UserSchema } from './schema/users.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersRepository],
+  exports: [UsersService, UsersRepository],
 })
 export class UsersModule {}
