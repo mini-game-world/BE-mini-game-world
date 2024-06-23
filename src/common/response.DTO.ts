415fa0f3332350c8fbd4eede1d9c07ff96089a3b
J-Jaeh <1wo2gur@naver.com>
2024-06-23 14:34:53 +0900

    #31 feat : 공용 response DTO 빌더패턴 적용

--- src/common/response.DTO.ts
+++ src/common/response.DTO.ts
@@ -1,12 +1,40 @@
-// 응답 DTO 객체 답아서 줌.
 export class ResponseDTO<T> {
   success: boolean;
   data?: T;
   message?: string;
 
-  constructor(success: boolean, data: T, message?: string) {
+  constructor(success: boolean, data?: T, message?: string) {
     this.success = success;
     this.data = data;
     this.message = message;
+  }
+
+  static builder<T>() {
+    return new ResponseDTOBuilder<T>();
+  }
+}
+
+class ResponseDTOBuilder<T> {
+  private success: boolean;
+  private data?: T;
+  private message?: string;
+
+  setSuccess(success: boolean): ResponseDTOBuilder<T> {
+    this.success = success;
+    return this;
+  }
+
+  setData(data: T): ResponseDTOBuilder<T> {
+    this.data = data;
+    return this;
+  }
+
+  setMessage(message: string): ResponseDTOBuilder<T> {
+    this.message = message;
+    return this;
+  }
+
+  build(): ResponseDTO<T> {
+    return new ResponseDTO(this.success, this.data, this.message);
   }
 }
