8fc29e0fcc934859e2abba6f34623c636328682a
J-Jaeh <1wo2gur@naver.com>
2024-06-23 15:46:43 +0900

    #31 feat : 유저 생성 로그 json.stringify 처리

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,25 +2,27 @@ import { Body, Controller, HttpCode, Logger, Post, UseFilters } from "@nestjs/co
 import { UsersService } from "./users.service";
 import { CreatCommonUserDTO } from "./DTO/users.creatDTO";
 import { ResponseDTO } from "../common/response.DTO";
-import { HttpExceptionFilter } from '../common/filters/http-exception.filter';
+import { HttpExceptionFilter } from "../common/filters/http-exception.filter";
+
 @Controller("users")
 export class UsersController {
   constructor(private readonly userService: UsersService) {
   }
+
   private logger: Logger = new Logger("Users - Controller");
 
   @Post("register")
   @HttpCode(201)
   @UseFilters(HttpExceptionFilter)
-  async registerUser(@Body() creatUserDTO: CreatCommonUserDTO):Promise<ResponseDTO<{ nickname: string }>> {
-    const userName:string = await this.userService.registerUser(creatUserDTO);
+  async registerUser(@Body() creatUserDTO: CreatCommonUserDTO): Promise<ResponseDTO<{ nickname: string }>> {
+    const userName: string = await this.userService.registerUser(creatUserDTO);
 
-    const response = ResponseDTO.builder<{ nickname: string }>()
+    const response:ResponseDTO<{ nickname: string }> = ResponseDTO.builder<{ nickname: string }>()
       .setSuccess(true)
       .setData({ nickname: userName })
       .build();
 
-    this.logger.log( `response==> ${response}`)
+    this.logger.log(`response registerUser==> ${JSON.stringify(response)}`);
 
     return response;
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
-import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
-import { UsersRepository } from './users.repository';
+import { HttpException, HttpStatus, Injectable, Logger } from "@nestjs/common";
+import { UsersRepository } from "./users.repository";
 import { CreatCommonUserDTO } from "./DTO/users.creatDTO";
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {
   }
+
   private logger: Logger = new Logger("Users - Service");
 
   async registerUser(createUserDto: CreatCommonUserDTO) {
@@ -15,7 +16,7 @@ export class UsersService {
     } catch (err) {
       this.logger.error(`registerUser  error massage ==> ${err.message}`);
 
-      throw new HttpException('해당 닉네임이 이미 존재합니다.', HttpStatus.CONFLICT);
+      throw new HttpException("해당 닉네임이 이미 존재합니다.", HttpStatus.CONFLICT);
     }
   }
 
