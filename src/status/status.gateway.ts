c7454695e3d9b4c13581309e7d34feb8fc374279
NOAH <flhbigfamily7@gmail.com>
2024-06-28 20:05:41 +0900

    -1마지막에 뜨는 오류 수정

--- src/status/status.gateway.ts
+++ src/status/status.gateway.ts
@@ -239,6 +239,7 @@ export class statusGateway implements OnGatewayInit, OnGatewayConnection, OnGate
           clearInterval(countdownInterval);
           if (this.isBombGameStart()) {
             this.bombGameStart();
+            return;
           }
         }
       }, 1000);
