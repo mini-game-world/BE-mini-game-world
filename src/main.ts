79ca8d25d3fc422b082390790e10c01fdb7934dd
NOAH <flhbigfamily7@gmail.com>
2024-06-29 10:57:35 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import * as http from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // HTTP 서버 인스턴스를 가져옵니다.
   const server = http.createServer(app.getHttpAdapter().getInstance());
 
+  // Geckos.io 서버를 초기화하고 HTTP 서버에 추가합니다.
   const { default: geckos } = await import('@geckos.io/server');
   const io = geckos({
     cors: {
@@ -16,12 +19,13 @@ async function bootstrap() {
 
   io.addServer(server);
 
+  // NestJS 앱 서버와 Geckos.io 서버가 같은 포트에서 실행되도록 설정합니다.
   await app.listen(3000, '0.0.0.0', () => {
     console.log('NestJS server listening on port 3000');
   });
 
-  server.listen(4000, () => {
-    console.log('Geckos.io server listening on port 4000');
+  server.listen(3000, () => {
+    console.log('Geckos.io server listening on port 3000');
   });
 }
 
