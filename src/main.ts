501e502de56d0cccba14c70dccb91962c9a37ea2
NOAH <flhbigfamily7@gmail.com>
2024-07-01 22:03:52 +0900

    https nginx없이 test

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as process from "process";
-import { CustomValidationPipe } from './common/pipes/custom-validation.pipe'
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/jungleptest.xyz/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/jungleptest.xyz/fullchain.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+  await app.listen(3000);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
