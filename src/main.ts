8c7b4995f7db9264d0a1f0ad6aa454512a7eec06
J-Jaeh <1wo2gur@naver.com>
2024-07-02 14:46:18 +0900

    fix : restore httpsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from "process";
-import { CustomValidationPipe } from './common/pipes/custom-validation.pipe'
 import * as fs from 'fs';
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync(process.env.HTTPS_KEY),
-  //   cert: fs.readFileSync(process.env.HTTPS_CERT),
-  // };
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.HTTPS_KEY),
+    cert: fs.readFileSync(process.env.HTTPS_CERT),
+  };
 
-  // const app = await NestFactory.create(AppModule, { httpsOptions });
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(process.env.PORT);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
