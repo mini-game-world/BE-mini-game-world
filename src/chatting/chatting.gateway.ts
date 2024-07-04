8d8a88896a58ad3b7fc8ea812c73d1a8f85a5a3a
NOAH <flhbigfamily7@gmail.com>
2024-07-04 22:54:23 +0900

    #59 feat : chatting.gateway geckos로 바꿈

--- src/chatting/chatting.gateway.ts
+++ src/chatting/chatting.gateway.ts
@@ -2,11 +2,8 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
-  SubscribeMessage,
   WebSocketGateway,
-  WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
 import { Logger } from '@nestjs/common';
 import { ChattingService } from './chatting.service.js';
 import { StatusBombGameService } from '../status/status.service.js';
@@ -16,43 +13,64 @@ export class ChattingGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   private logger: Logger = new Logger('Chatting-Gateway');
-
-  @WebSocketServer()
-  server: Server;
+  private io: any;
 
   constructor(
     private readonly chattingService: ChattingService,
     private readonly statusBombGameService: StatusBombGameService,
   ) {}
 
-  afterInit(server: any) {
-    this.logger.log('Init----Chatting-Gateway');
+  async afterInit(server: any) {
+    const geckosModule = await import('@geckos.io/server');
+    const geckos = geckosModule.default;
+
+    this.logger.log('Init');
+    const customIceServers = [
+      { urls: 'stun:stun.l.google.com:19302' },
+      { urls: 'stun:stun1.l.google.com:19302' }
+    ]
+    this.io = geckos({
+      iceServers: customIceServers
+    });
+    this.io.listen(3001, {
+      host: '0.0.0.0'  // 모든 네트워크 인터페이스에서 연결을 수락
+    });
+    this.logger.log(`server는??????????????${server}`);
+    // this.logger.log(`server는??????????????${JSON.stringify(server)}`);
+    // this.io.addServer(server);
+
+    this.io.onConnection((channel: any) => {
+      this.handleConnection(channel);
+    });
   }
 
-  handleConnection(client: any, ...args: any[]) {
-    this.logger.log(`chatting connect client.id --->${client.id}`);
+  handleConnection(channel: any) {
+    channel.on('message', (data: string) =>
+      this.handleMessage(channel, data),
+    );
+
+    channel.on('disconnect', () => this.handleDisconnect(channel));
   }
 
-  handleDisconnect(client: any) {
-    this.logger.log(`chatting disconnect client.id --->${client.id}`);
+  handleDisconnect(channel: any): any {
+    this.logger.log(`chatting disconnect client.id --->${channel.id}`);
   }
 
-  @SubscribeMessage('message')
-  async handleMessage(client: any, data: string) {
+  async handleMessage(channel: any, data: string) {
     if (!data) {
       this.logger.log(`message was not found.`);
       return;
     }
     const censoredMessage = await this.chattingService.censorBadWords(data);
-    const nickname = this.statusBombGameService.bombGameRoomPosition.get(client.id).nickname;
+    const nickname = this.statusBombGameService.bombGameRoomPosition.get(channel.id).nickname;
 
     const sendMessage: string =
       this.chattingService.checkChattingLen(censoredMessage);
 
     this.logger.log(`[Chatting message] ${nickname} : ${sendMessage} `);
 
-    this.server.emit('broadcastMessage', {
-      playerId: client.id,
+    this.io.emit('broadcastMessage', {
+      playerId: channel.id,
       nickname: nickname,
       message: sendMessage,
     });
