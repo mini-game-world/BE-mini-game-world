cf4e73e982c3c271e87a0e418714da925edc648e
J-Jaeh <1wo2gur@naver.com>
2024-07-04 11:21:28 +0900

    #69 test : item number 2 fix

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -329,7 +329,7 @@ export class StatusBombGameService {
 
         const responsePickupItem = ResponsePickUpItemDTO.builder()
           .setPlayerId(clientId)
-          .setItemNumber(0)
+          .setItemNumber(2)
           .setXDot(item.x)
           .setYDot(item.y)
           .build();
