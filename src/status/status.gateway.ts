cdadff8bf233704fa8aa0e816c72f706beef5e4c
J-Jaeh <1wo2gur@naver.com>
2024-07-06 17:01:31 +0900

    #68 feat : changed emit

--- src/status/status.gateway.ts
+++ src/status/status.gateway.ts
@@ -87,7 +87,7 @@ export class StatusGateway implements OnGatewayInit, OnGatewayConnection, OnGate
 
     // channel.emit('currentPlayers', Object.fromEntries(this.statusService.bombGameRoomPosition),);
     channel.emit('currentPlayers', this.waitingService.getAllWaitingRoomUser());
-    // channel.emit("gamestatus", this.bombGameStartFlag);
+    channel.emit("gamestatus", this.bombGameStartFlag);
 
     this.logger.log(`Client ${channel.id} joined`);
     this.logger.log(
