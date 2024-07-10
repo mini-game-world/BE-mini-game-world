6254bc7e261899a8f881a09badeeb78d8dbc0f6d
NOAH <flhbigfamily7@gmail.com>
2024-07-10 12:39:26 +0900

    #109 feat : 2.5초마다 맵줄어들도록 수정

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -365,7 +365,7 @@ export class StatusBombGameService {
     if (this.intervalId === null) { // 이미 실행 중인지 확인
       this.intervalId = setInterval(() => {
         this.mapShrink();
-      }, 2000); // 2초마다 실행
+      }, 2500); // 2.5초마다 실행
     }
   }
 
