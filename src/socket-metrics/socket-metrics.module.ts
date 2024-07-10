65f9f3a58bfe9196149dafa78e26017c906e2960
NOAH <flhbigfamily7@gmail.com>
2024-07-10 13:00:09 +0900

    test

--- src/socket-metrics/socket-metrics.module.ts
+++ src/socket-metrics/socket-metrics.module.ts
@@ -1,24 +1,24 @@
 import { Module } from '@nestjs/common';
 import { GeckosIoModule } from '../geckos/geckos.module.js';
-import { SocketMetricsGateway } from './socket-metrics.gateway.js';
+// import { SocketMetricsGateway } from './socket-metrics.gateway.js';
 import { makeCounterProvider, makeGaugeProvider, PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [GeckosIoModule, PrometheusModule.register()],
-  providers: [
-    SocketMetricsGateway,
-    makeCounterProvider({
-      name: 'websocket_connections_total',
-      help: '웹소켓 총 연결수 ',
-    }),
-    makeGaugeProvider({
-      name: 'active_users',
-      help: '현재 웹소켓 연결 수',
-    }),
-    makeCounterProvider({
-      name: 'websocket_disconnections_total',
-      help: '연결 해제 수 ',
-    }),
-  ],
+  // imports: [GeckosIoModule, PrometheusModule.register()],
+  // providers: [
+  //   SocketMetricsGateway,
+  //   makeCounterProvider({
+  //     name: 'websocket_connections_total',
+  //     help: '웹소켓 총 연결수 ',
+  //   }),
+  //   makeGaugeProvider({
+  //     name: 'active_users',
+  //     help: '현재 웹소켓 연결 수',
+  //   }),
+  //   makeCounterProvider({
+  //     name: 'websocket_disconnections_total',
+  //     help: '연결 해제 수 ',
+  //   }),
+  // ],
 })
 export class SocketMetricsModule {}
